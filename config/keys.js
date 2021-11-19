8f04d22684dea462ada95ffccc0f755639f0c326
Allan Cezar Almeida Chaves <allancac@gmail.com>
2021-11-19 20:28:29 -0300

    added the "devsp" collection to the mongodb URI

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 const mongoUser = process.env.MONGO_USER
 const mongoPassword = process.env.MONGO_PASSWORD
 module.exports = {
-  mongoDBURI: `mongodb+srv://${mongoUser}:${mongoPassword}@cluster0.3fd9f.mongodb.net/`
+  mongoDBURI: `mongodb+srv://${mongoUser}:${mongoPassword}@cluster0.3fd9f.mongodb.net/devsp`
 }
