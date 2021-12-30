9a3b069ec6108a3d9867b8332d491c2e83714bc5
Allan Cezar Almeida Chaves <allancac@gmail.com>
2021-12-29 21:18:07 -0300

    body-parser de terceiros trocado pelo nativo do ExpressJS

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const passport = require('passport');  // Importa o módulo passport
 const connectDB = require('./config/db');
 
-// Body Parser MiddleWare
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// MiddleWare do Body Parser nativo do ExpressJS
+app.use(express.json({ extended: false }));
 
 // Conexão com o banco de dados MongoDB
 connectDB();
@@ -16,17 +14,20 @@ app.use(passport.initialize()) // Inicializa o Passport no servidor "app"
 // chamada ao método, passando como parâmetro a variável pass( referência ao módulo  passport)
 require('./config/passport')(passport);
 
-//APIs routes
+//Importação das rotas da API
+const users = require('./routes/api/users');
 const auth = require('./routes/api/auth');
 const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
 
+// Disponibilização das rotas pelo servidor 
+app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use('/api/posts', posts);
 app.use('/api/profile', profile);
 
 // Server Listening Service
-const port = 5000 || process.env.PORT
+const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`Servidor funcionando na porta ${port}.`)
 
