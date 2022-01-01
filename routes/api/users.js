b96cd659050aba2a0bb923fd20a30205a2029efa
Allan Chaves <allancac@gmail.com>
2022-01-01 01:45:42 -0200

    Pacote validator trocado pelo nativo do ExpressJS

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,53 +3,61 @@ const router = express.Router();
 const User = require('../../models/User');  //Carregar o modelo de usuário - User
 const bcrypt = require('bcryptjs');   //Biblioteca para criptografar a senha do ususário
 const gravatar = require('gravatar'); //Biblioteca para obter imagem do avatar do e-mail fornecido
-const registerValidation = require('../../validation/register_validation')
+const {check, validationResult} = require('express-validator') //Biblioteca do validador nativo do ExpressJS
 
+//  Regras de validação da rota POST api/users
+const validacaoRegistroUsuario = [
+  check('nome', 'Forneça o nome completo do usuário').not().isEmpty(),
+  check('email', 'Forneça um e-mail válido').isEmail(),
+  check('senha', 'Forneça uma senha com 6 ou mais caracteres').isLength({min:6}),
+  check('senha2', 'Confirme a senha').not().isEmpty(),
+  check('senha2', 'As senhas não conferem').exists().custom((senha2, { req }) => senha2 === req.body.senha)
+]
 
 // @route   POST api/users
 // @desc    Rota para registrar usuario
 // @access  Public
-router.post('/', (req, res) => {
-  const { isValid, erros } = registerValidation(req.body);
-
-  if (!isValid) {
-    res.status(400).json(erros);
-  } else {
+router.post('/',validacaoRegistroUsuario,(req, res) => {
+  const erros = validationResult(req)
+  if(erros.isEmpty()){
     User.findOne({ email: req.body.email })
-      .then(user => {
-        // Validação do usuário. Aproveitando o objeto "erros" do método registerValidation()
-        if (user) {
-          erros.email = 'E-mail já cadastrado.' // cria uma key "email" no objeto erros
-          return res.status(400).json(erros);  // Resposta HTTP com objeto indicando o erro 
-        } else {
-          // Cria um avatar de acordo com o e-mail passado no body da requisição POST
-          const avatar = gravatar.url(req.body.email, {
-            s: '200', // tamanho
-            r: 'pg', // classificação
-            d: 'mm' // imagem de avatar padrão
+    .then(user => {
 
+      if (user) {
+        return res.status(400).json("Usuário já cadastrado.");  // Resposta HTTP com objeto indicando o erro 
+      } else {
+        // Cria um avatar de acordo com o e-mail passado no body da requisição POST
+        const avatar = gravatar.url(req.body.email, {
+          s: '200', // tamanho
+          r: 'pg', // classificação
+          d: 'mm' // imagem de avatar padrão
+
+        })
+
+        // Cria uma instância do Model User
+        const novoUsuario = new User({
+          nome: req.body.nome,
+          email: req.body.email,
+          avatar,
+          senha: req.body.senha
+        });
+        // Criptografa a senha do usuário para salvar no banco de dados
+        bcrypt.genSalt(10, (err, salt) => {  // Cria um "salt" com a senha fornecida
+          bcrypt.hash(novoUsuario.senha, salt, (err, hash) => { // Criptografa o salt gerado com a senha
+            if (err) throw err;
+            novoUsuario.senha = hash;
+            novoUsuario.save()  // Insere o usuário no banco de dados
+              .then(res.status(200).json({msg:"Usuário cadastrado"}))   // Resposta HTTP com o usuário criado no banco de dados MONGODB
+              .catch(err => console.log(err))
           })
-
-          // Cria uma instância do Model User
-          const novoUsuario = new User({
-            nome: req.body.nome,
-            email: req.body.email,
-            avatar,
-            senha: req.body.senha
-          });
-          // Criptografa a senha do usuário para salvar no banco de dados
-          bcrypt.genSalt(10, (err, salt) => {  // Cria um "salt" com a senha fornecida
-            bcrypt.hash(novoUsuario.senha, salt, (err, hash) => { // Criptografa o salt gerado com a senha
-              if (err) throw err;
-              novoUsuario.senha = hash;
-              novoUsuario.save()  // Insere o usuário no banco de dados
-                .then(usuario => res.json(usuario))   // Resposta HTTP com o usuário criado no banco de dados MONGODB
-                .catch(err => console.log(err))
-            })
-          });
-        }
-      }).catch(err => console.log(err))
+        });
+      }
+    })
+    .catch(err => console.log(err))
+  }else{
+    res.status(400).json({erros:erros.array()})
   }
+  
 });
 
 module.exports = router;
